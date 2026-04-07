797b38d359d0237a086dae4301e345c6b5ee4628
ak9451170-oss <ak9451170@gmail.com>
2026-04-07 09:22:46 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,32 @@
+//java program to illustrate if else statement
+public class ifelse {
+    public static void main(String[]args){
+        boolean a = true;
+        boolean b = false;
+
+        if(a){
+            System.out.println("a is true");
+        }
+        else{
+            System.out.println("a is false");
+        }
+        
+        if(b){
+            System.out.println("b is true");
+        }
+        else{
+            System.out.println("b is false");
+
+        
+
+        }
+        
+
+
+    }
+
+
+
+    
+    
+}
